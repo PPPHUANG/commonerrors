06543037daf8398eb0216ed2b04e1b07f4af2246
PPPHUANG <huangpengr@gmail.com>
2021-10-19 22:13:33 +0800

    feat: 新增备注

--- src/main/java/com/ppphuang/demo/reflect/javassist/test/UpdatePersonService.java
+++ src/main/java/com/ppphuang/demo/reflect/javassist/test/UpdatePersonService.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Method;
 
 /**
  * 修改已存在的JAVA文件
+ * insertBefore() 和 setBody()中的语句，如果你是单行语句可以直接用双引号，
+ * 但是有多行语句的情况下，你需要将多行语句用{}括起来。javassist只接受单个语句或用大括号括起来的语句块。
  */
 public class UpdatePersonService {
     public static void update() throws Exception {
