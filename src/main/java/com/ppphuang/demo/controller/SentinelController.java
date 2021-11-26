e0dd63cd1d7696665fe6fe113388239ae77aa04a
PPPHUANG <huangpengr@gmail.com>
2021-11-26 19:25:26 +0800

    feat: 新增sentinel测试

---
+++ src/main/java/com/ppphuang/demo/controller/SentinelController.java
@@ -0,0 +1,54 @@
+package com.ppphuang.demo.controller;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.ppphuang.demo.service.SentinelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "sentinel")
+public class SentinelController {
+
+    @Autowired
+    SentinelService sentinelService;
+
+    @GetMapping("test")
+    public String test() {
+        return sentinelService.hello(100L);
+    }
+
+    @GetMapping("test1")
+    public String test1() {
+        // 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
+        try (Entry entry = SphU.entry("HelloWorld")) {
+            // 被保护的逻辑
+            return "Hello World";
+        } catch (BlockException ex) {
+            // 处理被流控的逻辑
+            return "blocked!";
+        }
+    }
+
+    @PostConstruct
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
---
+++ src/main/java/com/ppphuang/demo/service/SentinelService.java
@@ -0,0 +1,26 @@
+package com.ppphuang.demo.service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentinelService {
+    // 原函数
+    @SentinelResource(value = "HelloWorld", blockHandler = "exceptionHandler", fallback = "helloFallback")
+    public String hello(long s) {
+        return String.format("Hello at %d", s);
+    }
+
+    // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+    public String helloFallback(long s) {
+        return String.format("Halooooo %d", s);
+    }
+
+    // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+    public String exceptionHandler(long s, BlockException ex) {
+        // Do some log here.
+        ex.printStackTrace();
+        return "Oops, error occurred at " + s;
+    }
+}
